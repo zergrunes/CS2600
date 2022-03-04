b3db455a59d0f3683a1b71765950eafc5e93fdcc
Marian Remoroza <mrremoroza@cpp.edu>
2022-03-03 21:28:46 -0800

    Merge branch 'main' of github.com:zergrunes/CS2600

---
+++ Project2/mealCost.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+On the first day of the trip, breakfast is allowed as an expense if the time of departure is before 7 a.m. 
+Lunch is allowed if the time of departure is before 12 noon. 
+Dinner is allowed on the first day if the time of departure is before 6 p.m. 
+On the last day of the trip, breakfast is allowed if the time of arrival is after 8 a.m. 
+Lunch is allowed if the time of arrival is after 1 p.m. 
+Dinner is allowed on the last day if the time of arrival is after 7 p.m. 
+
+The program should only ask for the amounts of allowable meals. 
+(The company allows up to $9 for breakfast, $12 for lunch, and $16 for dinner. Anything more than this must be paid by the employee.) 
+*/
+
+double breakfastCost(){
+    const double allowed_breakfast = 9.00;
+    double meal_fee;
+    printf("Did you eat breakfast before the alotted time of departure? (Y/N)");
+    
+
+}
+
+double lunchCost(){
+    const double allowed_lunch = 12.00;
+
+}
+
+double dinnerCost(){
+    const double allowed_dinner = 16.00;
+
+}
