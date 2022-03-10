f58d439c6e5694280bc308ab9f13a6a29009fdba
Marian Remoroza <mrremoroza@cpp.edu>
2022-03-10 02:18:31 -0800

    added author name

--- Quiz2/employeeMain.c
+++ Quiz2/employeeMain.c
@@ -1,3 +1,13 @@
+/**
+ * @file magicSquare.c
+ * @author Marian Remoroza
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <string.h>
 #include <stdlib.h>
 #include "employee.h"
---
+++ Quiz2/magicSquare.c
@@ -0,0 +1,14 @@
+/**
+ * @file magicSquare.c
+ * @author Marian Remoroza
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
